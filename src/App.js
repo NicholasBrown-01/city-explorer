6c3bb2bcf4e46c1392385c263a7c9f10c2d1dbc4
Nicholas <nicholasbrown.us@gmail.com>
2023-03-22 11:04:32 -0500

    Added syntax brackets

--- src/App.js
+++ src/App.js
@@ -40,105 +40,69 @@ class App extends React.Component {
       this.setState({
         error: true,
         errorMessage: error.message
-    })
-
-
-  getCityData = async (event) => {
-    event.preventDefault();
-
-    try {
-      // TODO: Use axios to get the data from LocationIQ - using city in state
-      let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
-
-      let cityDataFromAxios = await axios.get(url);
-
-      console.log(cityDataFromAxios.data[0])
-
-      // TODO: Set State with the data that comes back from axios & set error boolean to false
-      this.setState({
-        cityData: cityDataFromAxios.data[0],
-        error: false
       });
-
-    } catch (error) {
-
-      // TODO: Set state with the error boolean and the error message
-      this.setState({
-        error: true,
-        errorMessage: error.message
-      })
     }
-
   }
 
-  // *** MAP PORTION OF YOUR LAB IMG SRC POINTS TO THIS URL: 
-  // *** https://maps.locationiq.com/v3/staticmap?key=<YOUR API KEY>&center=<CITY'S LAT>,<CITY'S LON>&zoom=13
 
-  render() {
-    return (
-      <>
-        <h1>ENTER A CITY TO SEARCH FOR</h1>
+      getCityData = async (event) => {
+        event.preventDefault();
 
-        <form onSubmit={this.getCityData}>
-          <label > Enter in a City:
-            <input type="text" onChange={this.handleCityInput} />
-          </label>
-          <button type="submit">Explore!</button>
-        </form>
+        try {
+          // TODO: Use axios to get the data from LocationIQ - using city in state
+          let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
 
-        {/* https://tiles.locationiq.com/v3/<theme>/<type>.json?key=<access_token> */}
+          let cityDataFromAxios = await axios.get(url);
 
-        {
-          this.state.error
-            ? <p>{this.state.errorMessage}</p>
-            : <p>{this.state.cityData.display_name}
-                <ul>
-                  <img src={`https://tiles.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=12&size=400x400`} alt="Map of the city"/><br></br>
-                  {this.state.cityData.lat} {this.state.cityData.lon}
-                </ul>
-              </p>
+          console.log(cityDataFromAxios.data[0])
+
+          // TODO: Set State with the data that comes back from axios & set error boolean to false
+          this.setState({
+            cityData: cityDataFromAxios.data[0],
+            error: false
+          });
+
+        } catch (error) {
+
+          // TODO: Set state with the error boolean and the error message
+          this.setState({
+            error: true,
+            errorMessage: error.message
+          })
         }
-        
 
+      }
 
-      </>
-    )
-  }
-}
+      // *** MAP PORTION OF YOUR LAB IMG SRC POINTS TO THIS URL: 
+      // *** https://maps.locationiq.com/v3/staticmap?key=<YOUR API KEY>&center=<CITY'S LAT>,<CITY'S LON>&zoom=13
 
+      render() {
+        return (
+          <>
+            <h1>ENTER A CITY TO SEARCH FOR</h1>
 
+            <form onSubmit={this.getCityData}>
+              <label > Enter in a City:
+                <input type="text" onChange={this.handleCityInput} />
+              </label>
+              <button type="submit">Explore!</button>
+            </form>
 
+            {/* https://tiles.locationiq.com/v3/<theme>/<type>.json?key=<access_token> */}
 
-
-
-
-
-
-
-
-
-
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-export default App;
+            {
+              this.state.error
+                ? <p>{this.state.errorMessage}</p>
+                : <p>{this.state.cityData.display_name}
+                  <ul>
+                    <img src={`https://tiles.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=12&size=400x400`} alt="Map of the city" /><br></br>
+                    {this.state.cityData.lat} {this.state.cityData.lon}
+                  </ul>
+                </p>
+            }
+          </>
+        )
+      }
+    }
+    
+    export default App;
